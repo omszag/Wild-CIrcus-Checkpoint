b4963089421fd0d52a9c0acb39b350a96a26a181
omszag <53254676+omszag@users.noreply.github.com>
2020-01-29 10:57:30 +0100

    Add files via upload

---
+++ WildCircusCheckpoint/src/test/java/com/wildcodeschool/WildCircusCheckpointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WildCircusCheckpointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
