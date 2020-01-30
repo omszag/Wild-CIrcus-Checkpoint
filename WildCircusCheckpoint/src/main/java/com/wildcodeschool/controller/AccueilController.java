68098ca1ca054c72c009a24956d2b43f67708e25
omar <omszag@hotmail.fr>
2020-01-30 13:44:39 +0100

    sauvegarde

---
+++ WildCircusCheckpoint/src/main/java/com/wildcodeschool/controller/AccueilController.java
@@ -0,0 +1,16 @@
+package com.wildcodeschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AccueilController {
+	
+	@GetMapping("/")
+	public String home(Model model) {
+
+		return "index";
+	}
+
+}
---
+++ WildCircusCheckpoint/src/main/java/com/wildcodeschool/controller/ReservationController.java
@@ -0,0 +1,16 @@
+package com.wildcodeschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ReservationController {
+	
+	@GetMapping("/reservation")
+	public String reservation(Model model) {
+
+		return "reservation";
+	}
+
+}
